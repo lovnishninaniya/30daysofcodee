c4ca5c9be312eb52ea9c71e48f22d17fbabdf5e6
lovnishninaniya <94511409+lovnishninaniya@users.noreply.github.com>
2023-09-02 23:40:28 +0530

    Update findarray.js

--- Day2/findarray.js
+++ Day2/findarray.js
@@ -1,1 +1,12 @@
+const arr=[1,2,3,4,5,6];    
+function findOut(val){
+    for (let i = 0; i < arr.length;i++){
+        
+        if (arr[i]=== val){
+            console.log(`Number ${val} Found at index ${i}`); 
 
+        }
+        
+    };
+}
+findOut(4);
