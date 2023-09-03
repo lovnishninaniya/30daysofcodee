c975657ed8f8138186c2fa677866115cd364bdde
lovnishninaniya <94511409+lovnishninaniya@users.noreply.github.com>
2023-09-04 00:19:04 +0530

    Update DuplicateArray.js

--- Day3/DuplicateArray.js
+++ Day3/DuplicateArray.js
@@ -1,1 +1,5 @@
+const arr = [1, 2, 1, 3, 4, 3, 5];
 
+const toFindDuplicates = arr => arr.filter((item, index) => arr.indexOf(item) !== index)
+const duplicateElementa = tofindDuplicates(arry);
+console.log(duplicateElements);
