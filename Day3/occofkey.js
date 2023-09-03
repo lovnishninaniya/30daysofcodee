69b044eb3cccbf7149233fd2db899b7b9afb7c29
lovnishninaniya <94511409+lovnishninaniya@users.noreply.github.com>
2023-09-04 00:30:11 +0530

    Update occofkey.js

--- Day3/occofkey.js
+++ Day3/occofkey.js
@@ -1,1 +1,9 @@
+let key = 1;
+let A = [0, 1, 2, 3, 4, 5];
+let B = [5, 4, 3, 2, 1, 0];
 
+let occofKeyA = A.filter(item => item === key).length;
+console.log(`Key ${key} found ${ occofKeyA} time in  A`);
+
+let  occofKeyB = B.filter(item => item === key).length;
+console.log(`Key ${key} found ${ occofKeyB} time in  B`);
