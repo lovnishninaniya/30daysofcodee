607b3d9b111a744b2f90fe2a75c9c4d6c862770b
lovnishninaniya <94511409+lovnishninaniya@users.noreply.github.com>
2023-09-06 00:21:26 +0530

    Update oddeverarr.js

--- Day5/oddeverarr.js
+++ Day5/oddeverarr.js
@@ -1,1 +1,11 @@
-
+arr=[1,2,3,4,2,3,4];
+let oddArr=[];
+let evenArr=[];
+for (let i = 0;i<arr.length;i++){
+    if (arr[i]%2===0){
+        evenArr.push(arr[i])
+    }
+    else oddArr.push(arr[i])
+};
+console.log(`Odd Array :[${oddArr}]`)
+console.log(`Even Array :[${evenArr}]`)
