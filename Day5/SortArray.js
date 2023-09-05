85fd5b17f0112157a932403c0cf2289c51ea7c32
lovnishninaniya <94511409+lovnishninaniya@users.noreply.github.com>
2023-09-05 23:53:59 +0530

    Update SortArray.js

--- Day5/SortArray.js
+++ Day5/SortArray.js
@@ -1,1 +1,21 @@
+const quickSortt = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
 
+  let pivot = arr[0];
+  let leftArr = [];
+  let rightArr = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      leftArr.push(arr[i]);
+    } else {
+      rightArr.push(arr[i]);
+    }
+  }
+
+  return [...quickSortt(leftArr), pivot, ...quickSortt(rightArr)];
+};
+arr=[20,4,34,6,-1,8,3]
+quickSortt(arr)
