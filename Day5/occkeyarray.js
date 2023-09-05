f9e0b6787b24fcffe269edf0c86c450be717c954
lovnishninaniya <94511409+lovnishninaniya@users.noreply.github.com>
2023-09-06 00:20:19 +0530

    Update occkeyarray.js

--- Day5/occkeyarray.js
+++ Day5/occkeyarray.js
@@ -7,12 +7,12 @@ function occurence(arr, key) {
             return -1;
         }
 
-        const arr1 = [0, 1, 2, 3, 4, 5];
-        const arr2 = [5, 4, 3, 2, 1, 0];
+        const Arr1 = [0, 1, 2, 3, 4, 5];
+        const Arr2 = [5, 4, 3, 2, 1, 0];
         const key = 1;
 
-        const indexA = occurence(arr1, key);
-        const indexB = occurence(arr2, key);
+        const indexArr1 = occurence(arr1, key);
+        const indexArr2 = occurence(arr2, key);
 
-        console.log(`occurence of ${key} in arr1 at index: ${indexarr1}`);
-        console.log(`occurence of ${key} in arr2 at index: ${indexarr2}`);
+        console.log(`occurence of ${key} in arr1 at index: ${indexArr1}`);
+        console.log(`occurence of ${key} in arr2 at index: ${indexArr2}`);
