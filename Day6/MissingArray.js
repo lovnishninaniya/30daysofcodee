81372b3b5f5c21c1d05e0e4c2e71dc059fd51d94
lovnishninaniya <94511409+lovnishninaniya@users.noreply.github.com>
2023-09-06 22:31:38 +0530

    Update MissingArray.js

--- Day6/MissingArray.js
+++ Day6/MissingArray.js
@@ -1,1 +1,12 @@
+function findMissingNumbers(arr1, arr2) {
+  const set2 = new Set(arr2);
+  const missingNumbers = arr1.filter(num => !set2.has(num));
+  
+  return missingNumbers;
+}
 
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [2, 4, 6];
+
+const missingNumbers = findMissingNumbers(array1, array2);
+console.log(missingNumbers);
