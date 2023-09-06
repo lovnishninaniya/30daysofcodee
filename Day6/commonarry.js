9827cc242bc17849b686b6331368f8133b5c4984
lovnishninaniya <94511409+lovnishninaniya@users.noreply.github.com>
2023-09-06 22:27:25 +0530

    Update commonarry.js

--- Day6/commonarry.js
+++ Day6/commonarry.js
@@ -1,1 +1,19 @@
+function findCommonElements(array1, array2) {
+  const commonElements = [];
 
+  for (const element1 of array1) {
+    for (const element2 of array2) {
+      if (element1 === element2) {
+        commonElements.push(element1);
+        break; // Break to avoid adding duplicates
+      }
+    }
+  }
+
+  return commonElements;
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 4, 5, 6, 7];
+const result = findCommonElements(array1, array2);
+console.log(result);
